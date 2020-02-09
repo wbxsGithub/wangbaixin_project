454a0e37e88af006e81dddb91a469dd507d57c14
wbxsGithub <1493331881@qq.com>
2020-02-09 20:36:24 +0800

    maxSonOrder.cpp

--- SwordToOffer/main.cpp
+++ SwordToOffer/main.cpp
@@ -1,29 +1,35 @@
 #include <iostream>
+#include <stdlib.h>
 #include <vector>
-#include <queue>
+#include "BTree.h"
 using namespace std;
-
-int FindGreatestSumOfSubArray(vector<int> array) 
+int main()
 {
-	int sum = 0;
-	int max = array[0];
-	for (int i = 0; i < array.size();i++)
-	{
-		if (sum>0)
-		{
-			sum += array[i];
-		}
-		else
-		{
-			sum = array[i];
-		}
-		max = (max > sum ? max : sum);
-	}
-	return max;
-}
-void main()
-{
-	vector<int> array = { 6, -3, -2, 7, -15, 1, 2, 2 };
-	cout<<FindGreatestSumOfSubArray(array);
-}
+	vector<int> arr = { 5, 3, 7, 9, 8, 2, 1, 6, 4 };
+	
+	BTree bt;//创建该类的对象
+	bt.buildBinaryTree(arr);//创建二叉搜索树
+	Node *root = bt.getRoot();
+#pragma region //先、中、后遍历二叉树
 
+	bt.preorder(root);
+	cout << endl;
+
+	bt.inorder(root);
+	cout << endl;
+	
+	bt.postorder(root);
+	cout << endl;
+
+#pragma endregion 
+	bt.layerorder(root);
+//#pragma region //找key值
+//	cout << "查找key值,并输出其lchild的val:" << endl;
+//	Node *node = bt.searchNode(root, 3);
+//	cout << node->lchild->val << endl;
+//#pragma endregion 
+//	cout << bt.max(root)<<endl;
+//	cout << bt.min(root) << endl;
+//	bt.destroyTree(root);
+	return 0;
+}
